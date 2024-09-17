07b4d3d3a0569491e87063efacd5c735e26c95b3
Boris <mssync99999@gmail.com>
2024-09-17 20:21:21 +0300

    1709-1

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,21 +1,48 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.service.PostService;
+import ru.yandex.practicum.catsgram.service.UserService;
 
 import java.time.Instant;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+    private final PostService postService;
+    private final UserService userService;
+    //private final Map<Long, Post> posts = new HashMap<>();
 
-    private final Map<Long, Post> posts = new HashMap<>();
+    //@Autowired
+    public PostController(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
+    }
 
+    @GetMapping
+    public Collection<Post> findAll() {
+        return postService.findAll();
+    }
+
+    @PostMapping
+    public Post create(@RequestBody Post post) {
+        return postService.create(post, userService);
+    }
+
+    @PutMapping
+    public Post update(@RequestBody Post newPost) {
+        return postService.update(newPost);
+    }
+
+    /*
     @GetMapping
     public Collection<Post> findAll() {
         return posts.values();
@@ -62,4 +89,6 @@ public class PostController {
                 .orElse(0);
         return ++currentMaxId;
     }
+
+     */
 }
--- src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
@@ -1,47 +1,31 @@
-package ru.yandex.practicum.catsgram.controller;
+package ru.yandex.practicum.catsgram.service;
 
-
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
-import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.User;
-import ru.yandex.practicum.catsgram.model.Post;
-import ru.yandex.practicum.catsgram.service.PostService;
-import ru.yandex.practicum.catsgram.service.UserService;
 
 import java.time.Instant;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
-@RestController
-@RequestMapping("/users")
-public class UserController {
-    private final UserService userService;
-    //private final Map<Long, Post> posts = new HashMap<>();
+@Service
+public class UserService {
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-
-    @GetMapping
-    public Collection<User> findAll() {
-        return userService.findAll();
-    }
-
-    @PostMapping
-    public User create(@RequestBody User user) {
-        return userService.create(user);
-    }
-
-    @PutMapping
-    public User update(@RequestBody User newUser) {
-        return userService.update(newUser);
-    }
-    /*
     private final Map<Long, User> users = new HashMap<>();
+
+    /*    Long id; //— уникальный идентификатор пользователя,
+    String username; //— имя пользователя,
+    String email; //— электронная почта пользователя,
+    String password; //— пароль пользователя,
+    Instant registrationDate; //— дата и время регистрации.*/
+
 
     @GetMapping
     public Collection<User> findAll() {
@@ -122,5 +106,15 @@ public class UserController {
 
         return ++currentMaxId;
     }
-*/
+
+    public Optional<User> findUserById(long authorId) {
+        for (User entry : users.values()) {
+            //System.out.println(entry);
+            if (entry.getId() == authorId) {
+                return Optional.of(entry);
+            }
+        }
+        return Optional.empty();
+    }
+
 }
